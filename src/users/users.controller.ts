a22d2c793e9276ce3e2e74c5df5a442a146aad07
Auron <98396887+auronvila@users.noreply.github.com>
2023-10-03 18:13:22 +0300

    exclude the password in the user res service body

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseInterceptors
+} from "@nestjs/common";
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { ApiBody, ApiTags } from "@nestjs/swagger";
 import { UsersService } from "./users.service";
 import { UpdateUserDto } from "./dtos/update-user.dto";
-import { optionalRequire } from "@nestjs/core/helpers/optional-require";
+
 
 @ApiTags("auth")
 @Controller("auth")
@@ -17,6 +27,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get("/:id")
   findUser(@Param("id") id: string) {
     return this.usersService.findById(parseInt(id));
